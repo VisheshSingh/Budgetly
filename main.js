e07ef02b637582faa17daeac46969806c1c500a0
Vishesh <visheshsingh16@gmail.com>
2018-12-21 15:03:40 -0500

    Budget form

--- main.js
+++ main.js
@@ -15,4 +15,65 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  // Submit Budget Form
+  submitBudgetForm() {
+    const value = this.budgetInput.value;
+    if (value == "" || value < 0) {
+      this.budgetFeedback.classList.add("showItem");
+      this.budgetFeedback.innerHTML = `<p>Value cannot be empty or negative</p>`;
+      setTimeout(() => {
+        this.budgetFeedback.classList.remove("showItem");
+      }, 4000);
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+      this.showBalance();
+    }
+  }
+
+  // show balance
+  showBalance() {
+    const budgetAmt = Number(this.budgetAmount.textContent);
+    const expenseAmt = Number(this.expenseAmount.textContent);
+    const bal = budgetAmt - expenseAmt;
+    this.balanceAmount.textContent = bal;
+
+    if (bal < 0) {
+      this.balance.classList.add("showRed");
+      this.balance.classList.remove("showGreen", "showBlack");
+    } else if (bal > 0) {
+      this.balance.classList.add("showGreen");
+      this.balance.classList.remove("showRed", "showBlack");
+    }
+    if (bal === 0) {
+      this.balance.classList.add("showBlack");
+      this.balance.classList.remove("showGreen", "showRed");
+    }
+  }
 }
+
+function eventListeners() {
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  //new instance of UI
+  const ui = new UI();
+
+  //budget form submit
+  budgetForm.addEventListener("submit", e => {
+    e.preventDefault();
+    ui.submitBudgetForm();
+  });
+  //expense form submit
+  expenseForm.addEventListener("submit", e => {
+    e.preventDefault();
+  });
+  //expense list submit
+  expenseList.addEventListener("click", e => {});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  eventListeners();
+});
