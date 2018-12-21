2be24d9f653c4225119d423b8a6a460e37aa5b2d
Vishesh <visheshsingh16@gmail.com>
2018-12-21 15:36:01 -0500

    Adding items to expense list

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ class UI {
   // show balance
   showBalance() {
     const budgetAmt = Number(this.budgetAmount.textContent);
-    const expenseAmt = Number(this.expenseAmount.textContent);
+    const expenseAmt = this.totalExpense();
     const bal = budgetAmt - expenseAmt;
     this.balanceAmount.textContent = bal;
 
@@ -50,6 +50,66 @@ class UI {
       this.balance.classList.add("showBlack");
       this.balance.classList.remove("showGreen", "showRed");
     }
+  }
+
+  // Submit Budget Form
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+
+    if (expenseValue == "" || amountValue == "" || amountValue < 0) {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML = `<p>Value cannot be empty or negative</p>`;
+      setTimeout(() => {
+        this.expenseFeedback.classList.remove("showItem");
+      }, 4000);
+    } else {
+      let amount = Number(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount
+      };
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      // show balance
+      this.showBalance();
+    }
+  }
+
+  // add expense
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+            <h6 class="expense-title mb-0 text-uppercase list-item">${
+              expense.title
+            }</h6>
+            <h5 class="expense-amount mb-0 list-item">- ${expense.amount}</h5>
+            <div class="expense-icons list-item">
+                <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <a href="#" class="delete-icon" data-id="${expense.id}">
+                    <i class="fas fa-trash"></i>
+                </a>
+            </div>
+        </div>`;
+    this.expenseList.appendChild(div);
+  }
+
+  totalExpense() {
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce((amt, item) => amt + item.amount, 0);
+    }
+    this.expenseAmount.textContent = total;
+    return total;
   }
 }
 
@@ -69,6 +129,7 @@ function eventListeners() {
   //expense form submit
   expenseForm.addEventListener("submit", e => {
     e.preventDefault();
+    ui.submitExpenseForm();
   });
   //expense list submit
   expenseList.addEventListener("click", e => {});
